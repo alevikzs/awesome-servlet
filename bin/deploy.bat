486fd06f34d53c47eef70f09061ef65ce58b7646
Alexey Novikov <alekseeey@gmail.com>
2017-07-18 20:37:12 +0300

    added maven

--- bin/deploy.bat
+++
@@ -1,5 +0,0 @@
-   javac -d web\WEB-INF\classes\ src\TestingServlet.java
-   jar -cvf target\alevikzs.war -C web .
-   copy target\alevikzs.war "%CATALINA_HOME%\webapps\alevikzs.war"
-   net stop tomcat9
-   net start tomcat9
--- src/main/java/org/awesome/servlet/AwesomeServlet.java
+++ src/main/java/org/awesome/servlet/AwesomeServlet.java
@@ -1,13 +1,15 @@
+package org.awesome.servlet;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
-import java.util.*;
 
-public class TestingServlet extends HttpServlet {
+public class AwesomeServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
 
-        out.println("Awesome testing!");
+        out.println("Awesome testing!!!");
     }
+
 }
