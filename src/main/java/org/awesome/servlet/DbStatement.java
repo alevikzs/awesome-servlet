8480cb507ba7daea50cc158867822f028ecf8656
Alexey Novikov <alekseeey@gmail.com>
2017-09-07 18:34:21 +0300

    fixed mysql driver

--- src/main/java/org/awesome/servlet/DbStatement.java
+++ src/main/java/org/awesome/servlet/DbStatement.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class DbStatement {
 
-    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost/awesomeServlet";
     private static final String USER = "root";
     private static final String PASSWORD = "";
